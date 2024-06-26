54e633b4e985df956d9437724a866264edc9e0b0
matheusfvesco <114014793+matheusfvesco@users.noreply.github.com>
2024-06-26 19:46:27 -0300

    Simplified tensorflow-gpu

--- test/tensorflow-gpu/test.sh
+++ test/tensorflow-gpu/test.sh
@@ -4,12 +4,23 @@ cd $(dirname "$0")
 source test-utils.sh vscode
 
 checkCommon
+check "sudo" sudo echo "sudo works."
+check "git" git --version
+check "git-location" sh -c "which git | grep /usr/local/bin/git"
+check "zsh" zsh --version
+
+# python related tests
 check "python" python --version
 check "pipx" pipx --version
+check "poetry" poetry --version
+check "monkeytype" monkeytype -h
+check "mypy" mypy --version
+check "bandit" bandit --version
+
+# gpu access related tests
 check "nvidia-smi" nvidia-smi
 check "nvcc" nvcc --version
 check "cudnn" bash -c '[ -n "$(whereis cudnn.h | grep -o /)" ]'
-check "git" git --version
 check "tensorflow GPU is available" python -c "import tensorflow as tf, sys; sys.exit(0) if len(tf.config.list_physical_devices('GPU')) > 0 else sys.exit(1)"
 
 # Report result
