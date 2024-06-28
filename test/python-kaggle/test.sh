31c8844fae469decd1d2cbba763db6909afc9276
matheusfvesco <114014793+matheusfvesco@users.noreply.github.com>
2024-06-27 23:24:18 -0300

    Added python-kaggle template

---
+++ test/python-kaggle/test.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+cd $(dirname "$0")
+
+source test-utils.sh vscode
+
+checkCommon
+checkCommonNonRoot
+
+# python related tests
+checkCommonPython
+
+# gpu access related tests
+checkCommonBasicGPU
+checkCompleteCuda
+check "pytorch cuda is available" python -c "import torch, sys; sys.exit(0) if torch.cuda.is_available() else sys.exit(1)"
+check "tensorflow GPU is available" python -c "import tensorflow as tf, sys; sys.exit(0) if len(tf.config.list_physical_devices('GPU')) > 0 else sys.exit(1)"
+
+# Report result
+reportResults
