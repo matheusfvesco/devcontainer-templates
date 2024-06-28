b0625fc8a786e18df5164d12e530e031fee00f33
matheusfvesco <114014793+matheusfvesco@users.noreply.github.com>
2024-06-27 21:27:19 -0300

    Renamed python-pipx to python-utils and refactored it

--- test/python-utils/test.sh
+++ test/python-utils/test.sh
@@ -6,8 +6,8 @@ source test-utils.sh vscode
 checkCommon
 checkCommonPython
 checkCommonPythonTools
-checkCommonBasicGPU
-checkCompleteCuda
+check "python path" bash -c "[ -f /usr/local/bin/python ]"
+
 
 # Report result
 reportResults
