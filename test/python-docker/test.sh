a356f406af911cbd1d37332b873bd563bdf4ff9f
matheusfvesco <114014793+matheusfvesco@users.noreply.github.com>
2024-06-27 21:30:23 -0300

    Renamed python package dev to python-docker and refactored it, removed pithon-requirements

---
+++ test/python-docker/test.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+cd $(dirname "$0")
+
+source test-utils.sh vscode
+
+checkCommon
+checkCommonPython
+checkCommonPythonTools
+check "python path" bash -c "[ -f /usr/local/bin/python ]"
+
+check "docker" which docker
+check "docker alpine" docker pull --quiet alpine
+
+# Report result
+reportResults
